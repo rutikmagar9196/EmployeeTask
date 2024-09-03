ce2c46611b1f25aa086fd9f0fd719f58afab3eb7
rutikmagar9196 <rutikmagar9196@gmail.com>
2024-09-03 23:01:11 +0530

    my app employee task 1st commit into git

---
+++ BusinessLogic/ExtraLogic.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.UI.WebControls.WebParts;
+using EmployeeTask.Models;
+
+namespace EmployeeTask.BusinessLogic
+{
+
+    public class ExtraLogic
+    {
+        EmployeeTaskDbEntities db;
+        public ExtraLogic()
+        {
+            db = new EmployeeTaskDbEntities();
+        }
+
+        public string NextEmployeeCode()
+        {
+            string code = "E";
+            if (db.tblemployees.ToList().Count > 0)
+            {
+
+                tblemployee e = db.tblemployees.ToList().OrderByDescending(ep => ep.Emp_Id).First();                
+                
+               if (e.Emp_Id >= 1 && e.Emp_Id < 10)
+               {
+                  code = "E000" + (e.Emp_Id + 1);
+               }
+               else if (e.Emp_Id >= 10 && e.Emp_Id < 100)
+               {
+                  code = "E00" + (e.Emp_Id + 1);
+               }
+               else if (e.Emp_Id >= 100 && e.Emp_Id < 1000)
+               {
+                  code = "E0" + (e.Emp_Id + 1);
+               }
+               else if (e.Emp_Id >= 1000 && e.Emp_Id < 10000)
+               {
+                  code = "E" + (e.Emp_Id + 1);
+               } 
+            }
+            else
+            {
+                code = "E0001"; 
+            }
+            return code;
+        }
+        public string GeneratePassword(int size)
+        {
+            string data = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ1234567890@#$";
+            string password = "";
+            Random random = new Random();
+            for (int i = 1; i <= size; i++ ) 
+            {
+                int p=random.Next(0,data.Length-1);
+                password += data[p];
+
+            }
+            return password;
+            
+
+        }
+        public string GenerateOTP(int size)
+        {
+            string data = "0123456789";
+            string OTP = "";
+            Random random = new Random();
+            for (int i = 1; i <= size; i++)
+            {
+                int p = random.Next(0, data.Length - 1);
+                OTP += data[p];
+
+            }
+            return OTP;
+
+
+        }
+
+        public static void Send_Email(string email, string subject, string discription)
+        {
+            var fromAddress = new MailAddress("rutikmagar9196@gmail.com", "Montcreast Software Pvt.Ltd");
+            var toAddress = new MailAddress(email, email);
+            MailMessage meassage = new MailMessage(fromAddress, toAddress);
+            meassage.Subject = subject;
+            meassage.Body = discription;
+            meassage.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            NetworkCredential networkCred = new NetworkCredential();
+            networkCred.UserName = "rutikmagar9196@gmail.com";
+            networkCred.Password = "pqad zlzj ovyg kxue";
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = networkCred;
+            smtp.Port = 587;
+            smtp.Send(meassage);
+
+        }
+        /*public string Send_Email(string email, string subject, string description)
+        {
+            try
+            {
+                var fromAddress = new MailAddress("rutikmagar9196@gmail.com", "Montcreast Software Pvt.Ltd");
+                var toAddress = new MailAddress(email, email);
+                MailMessage message = new MailMessage(fromAddress, toAddress);
+                message.Subject = subject;
+                message.Body = description;
+                message.IsBodyHtml = true;
+
+                SmtpClient smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("rutikmagar9196@gmail.com", "pqad zlzj ovyg kxue"),
+                    Port = 587
+                };
+
+                smtp.Send(message);
+
+                return "Email sent successfully";
+            }
+            catch (Exception ex)
+            {
+                return $"Failed to send email: {ex.Message}";
+            }
+        }*/
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeTask.Models;
+using System.IO;
+using EmployeeTask.BusinessLogic;
+
+namespace EmployeeTask.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeTaskDbEntities db;
+        ExtraLogic ebl;
+        public EmployeeController()
+        {
+            db = new EmployeeTaskDbEntities();
+            ebl = new ExtraLogic();
+        }
+            
+
+        public ActionResult Register()
+        {
+            string code = ebl.NextEmployeeCode();
+            tblemployee e = new tblemployee() { Employee_Code = code };            
+
+            return View(e);
+        }
+
+        [HttpPost]
+        public ActionResult Resister(tblemployee e, HttpPostedFileBase photo)
+        {
+            if (photo != null)
+            {
+                string imgname = e.Emp_Name + Path.GetExtension(photo.FileName);
+                string imgpath = Server.MapPath("~/Profile Photo/" + imgname);
+                photo.SaveAs(imgpath);
+                e.Profile_Photo = imgname;
+            }
+            string password = ebl.GeneratePassword(10);
+            e.password = password;
+            db.tblemployees.Add(e);
+            db.SaveChanges();
+            string msg = "<h2>Dear " + e.Emp_Name + "</h2>,<p>your Account has been created successfully</p><p> You Can Access Your Account By Following Creadential.</p><p> Employee Code:<b> " + e.Employee_Code + "</b></br> Password:<b> " + e.password + "</b> <br/></p><p><br/><br/><br/><br/><br/><br/><h4>Thanks & Regards</h4><h4>Rutik Magar</h4></p>";
+            string subject = "Employee Account Registration";
+            ExtraLogic.Send_Email(e.Email_Address, msg, subject);
+            ViewBag.msg = "your Account has been created successfully,You Can Access You Mail For Frither Process";
+            ModelState.Clear();
+            string code = ebl.NextEmployeeCode();
+            tblemployee em = new tblemployee() { Employee_Code = code };
+            return View(em);
+
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+    }
+}
